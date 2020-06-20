204e8d2ac0625ae936f3b729ffcfe1ac953a3988
boeing73 <67189120+boeing73@users.noreply.github.com>
2020-06-20 03:44:11 -0700

    Añadida explicacion de suma

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    // los signos + antes de cada operando son necesarios para evitar concatenacion
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
